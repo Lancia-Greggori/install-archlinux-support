4b0d925dac1b6ec85340bb0c7bfe421de89750ba
Lancia <lanciagreggori@gmail.com>
2022-07-31 17:14:00 +0430

    Completed v1.0 of the program

--- install-archlinux-support.sh
+++ install-archlinux-support.sh
@@ -1,36 +1,38 @@
 #!/bin/sh
 
-set -eux
-
-NEWLINE='
-'
-
-wget 'https://wiki.artixlinux.org/Main/Repositories' -O repos.html
-
-awk '/<pre> *\[universe\]/,/<\/pre>/' repos.html | grep -o '>.\+</a>' | sed -e 's/>//g; s/<\/a>*//g; s/^/Server = /; 1i [universe]' -e "\$a$NEWLINE" >universe-repos.txt
-
-GALAXY_DIRECTIVE_LINE_NUMBER="$(grep -Fn '[galaxy]' /etc/pacman.conf | cut -d':' -f1)"
-
-LINE_NUM="$((GALAXY_DIRECTIVE_LINE_NUMBER + 1))"
+set -eu
 
 
-# using the GALAXY_DIRECTIVE_LINE_NUMBER variable defined above, start at the next line number (LINE_NUM) after the "[galaxy]" directive 
+trap 'cp /etc/pacman.conf.orig /etc/pacman.conf;  rm -f repos.html arch-repos.txt universe-repos.txt' INT
 
-printf 'Adding Universe repos to /etc/pacman.conf...'
 
-if grep -F '[universe]' /etc/pacman.conf 1>/dev/null; then
+# Make a backup of /etc/pacman.conf
 
-	printf '\tthe [universe] directive already exists in /etc/pacman.conf, skipping this step...\n'
+echo 'Making a copy of /etc/pacman.conf into /etc/pacman.conf.orig...'
 
-else
+cp -i /etc/pacman.conf /etc/pacman.conf.orig
+
+
+
+# Define some useful functions 
+
+# Arg1: the directive to add after
+# Arg2: the file that contains the repos to be added
+
+add_repos()
+{
+
+	# Find the line number that contains the desired directive, and start placing the repos after that
+
+	LINE_NUM="$(( $(grep -Fn "$1" /etc/pacman.conf | cut -d':' -f1) + 1 ))"
 
 	while true; do
 	
 		# See if the line does not start with a "Server" or "Include" keyword
 	
-		if ! sed -n "${LINE_NUM}p" /etc/pacman.conf | grep -E '^(Include|Server)'; then
+		if ! sed -n "${LINE_NUM}p" /etc/pacman.conf | grep -E '^(Include|Server)' 1>/dev/null; then
 	
-			sed -i'' "${LINE_NUM}r universe-repos.txt" /etc/pacman.conf
+			sed -i'' "${LINE_NUM}r $2" /etc/pacman.conf
 
 			break
 	
@@ -42,16 +44,64 @@ else
 	
 	done
 
-fi
+}
+
+sync_with_repos()
+{
+
+	if ! pacman -Sy; then
+	
+		echo 'Error: failed to sync using "pacman -Syu"'
+	
+		exit 1
+	
+	fi
+
+}
 
 
-if ! pacman -Sy; then
+# get the latest universe repos
 
-	echo 'Error: failed to sync using "pacman -Syu"'
+URL='https://wiki.artixlinux.org/Main/Repositories'
+
+echo 'Getting the latest Universe repos...'
+
+if wget -q "$URL" -O repos.html; then
+
+	NEWLINE='
+	'
+
+	awk '/<pre>.*\[universe\]/,/<\/pre>/' repos.html | grep -o '>.\+</a>' \
+		| sed -e 's/>//g; s/<\/a>*//g; s/^/Server = /; 1i [universe]' -e "\$a$NEWLINE" > universe-repos.txt
+
+else
+
+	echo "Error: failed to retrieve the list of repositories from $URL"
 
 	exit 1
 
 fi
+
+
+# Add the universe repos
+
+echo 'Adding the Universe repos to /etc/pacman.conf...'
+
+if grep -F '[universe]' /etc/pacman.conf 1>/dev/null; then
+
+	printf '\tThe [universe] directive already exists in /etc/pacman.conf, skipping this step...\n'
+
+else
+
+	add_repos '[galaxy]' 'universe-repos.txt'
+
+fi
+
+
+sync_with_repos
+
+
+echo 'Installing artix-archlinux-support...'
 
 if ! pacman -S artix-archlinux-support; then
 
@@ -60,3 +110,31 @@ if ! pacman -S artix-archlinux-support; then
 	exit 1
 
 fi
+
+
+# Add the Arch repos
+
+echo 'Adding Arch repos...'
+
+awk '/^ *\[extra\]/,/<\/pre>/' repos.html | sed '/<\/pre>/d; s/^ \+//' > arch-repos.txt
+
+add_repos '[universe]' 'arch-repos.txt'
+
+
+
+echo 'Running "pacman-key --populate archlinux"...'
+
+if ! pacman-key --populate archlinux; then
+
+	echo 'Error: failed in running command "pacman-key --populate archlinux"'
+
+fi
+
+
+sync_with_repos
+
+
+rm repos.html universe-repos.txt arch-repos.txt
+
+
+echo 'Arch repositories have been added successfully, Have a nice day!'
